9f2395f4524bbed9c0288c555a6f8b5c43b5ca13
unknown <tiranpara@gmail.com>
2020-01-03 13:35:48 +0400

    Initial commit

---
+++ src/components/clients/index.js
@@ -0,0 +1,137 @@
+import React, { PureComponent } from 'react';
+import {Cell, Column, Table, ColumnHeaderCell} from "@blueprintjs/table";
+import { dummyData } from "../../data";
+import {Menu, MenuItem, HTMLSelect, Button} from "@blueprintjs/core";
+
+import './styles.scss';
+
+export default class Clients extends PureComponent {
+
+    itemsPerPage = 15;
+    endRecords = this.itemsPerPage;
+    selectOptions = [{ value: 15, label: '15 Records per page' }, {value: 30, label: '30 Records per page'}];
+
+    state = {
+        dummyData: dummyData.slice(0, this.itemsPerPage).map(data => {
+            const { id, ...rest } = data;
+            return rest;
+        })
+    };
+
+    sortColumns = (columnname, order) => {
+        this.setState({
+            dummyData: [...this.state.dummyData].sort((prev, curr) => {
+                if(order === 'asc') {
+                    return prev[columnname] > curr[columnname] ? 1 : prev[columnname] === curr[columnname] ? 0 : -1
+                } else {
+                    return prev[columnname] < curr[columnname] ? 1 : prev[columnname] === curr[columnname] ? 0 : -1
+                }
+
+            })
+        })
+    };
+
+    renderColumns = () => {
+        const columns = [];
+        Object.keys(this.state.dummyData[0]).forEach(columnname => {
+
+            const menuRenderer = columnIndex => {
+                return (
+                    <Menu>
+                        <MenuItem icon="sort-asc" text="Sort Asc" onClick={() => this.sortColumns(columnname, 'asc')} />
+                        <MenuItem icon="sort-desc" text="Sort Desc" onClick={() => this.sortColumns(columnname, 'desc')} />
+                    </Menu>
+                );
+            };
+
+            const columnHeaderCellRenderer = () => (
+                <ColumnHeaderCell className='header-cell' name={columnname} menuRenderer={menuRenderer}  />
+            );
+
+
+            columns.push(
+                <Column
+                    className='blueprint-column'
+                    key={columnname}
+                    name={columnname}
+                    columnHeaderCellRenderer={columnHeaderCellRenderer}
+                    cellRenderer={this.renderCell}
+                />
+            );
+        });
+
+        return columns;
+    };
+
+    renderCell = (rowIndex, columnIndex) => {
+        const rowData = this.state.dummyData[rowIndex];
+        console.log(this.state.dummyData)
+        const keyNameOfCurrentColumn = Object.keys(rowData)[columnIndex];
+        const valueOfCurrentColumn = rowData[keyNameOfCurrentColumn];
+
+        return <Cell className='table-cell'>{valueOfCurrentColumn}</Cell>;
+    };
+
+    changeHandler = (e) => {
+        this.itemsPerPage = Number(e.target.value);
+        this.endRecords = this.itemsPerPage;
+        this.setState({
+            dummyData: dummyData.slice(0, this.endRecords).map(data => {
+                const { id, ...rest } = data;
+                return rest;
+            })
+        })
+    };
+
+    loadMoreHandler = () => {
+        this.endRecords = this.itemsPerPage + this.endRecords > dummyData.length ? dummyData.length : this.itemsPerPage + this.endRecords;
+        this.setState({
+            dummyData: dummyData.slice(
+                0,
+                this.endRecords
+            ).map(data => {
+                const { id, ...rest } = data;
+                return rest;
+            })
+        })
+    };
+
+    render() {
+        return (
+            <div className='clients-container'>
+                <h2>Clients</h2>
+                <Table
+                    selectionModes='NONE'
+                    className='blueprint-table bp3-table-striped'
+                    defaultColumnWidth={400}
+                    defaultRowHeight={35}
+                    numRows={this.state.dummyData.length}
+                    enableColumnResizing={false}
+                    enableMultipleSelection={false}
+                    enableRowHeader={false}
+                    enableRowResizing={false}
+                >
+                    {
+                        this.renderColumns()
+                    }
+                </Table>
+                <div className="paging-container">
+                    <div>
+                        <HTMLSelect
+                            options={this.selectOptions}
+                            onChange={this.changeHandler}
+                        />
+                    </div>
+                    <div className='info-group'>
+                        <div>
+                            1 - {this.endRecords > dummyData.length || this.itemsPerPage > dummyData.length ? dummyData.length : this.endRecords}
+                            of {dummyData.length}
+                        </div>
+                        <Button onClick={this.loadMoreHandler}>Load More</Button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+}
---
+++ src/data/index.js
@@ -0,0 +1,105 @@
+import moment from 'moment';
+
+export const dummyData = [{
+    id: 1,
+    name: 'John doe',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+},{
+    id: 2,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment('12312019', 'MMDDYYYY').fromNow(),
+    source: 'Fms ad'
+}, {
+    id: 3,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 4,
+    name: 'Claudia Hosano',
+    status: 'Open - Not Contacted',
+    modified: moment().fromNow(),
+    source: 'SUp ad'
+}, {
+    id: 5,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 6,
+    name: 'Claudia Carvajal',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 7,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 8,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 9,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 10,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 11,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 12,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 13,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 14,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 15,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 16,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}, {
+    id: 17,
+    name: 'Claudia Hosano',
+    status: 'Working - Contacted',
+    modified: moment().fromNow(),
+    source: 'Google ad'
+}];
